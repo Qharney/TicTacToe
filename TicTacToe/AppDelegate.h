6e8a33e9a50b9263808391784a2fb92dd6bc94eb
Quinntrell Harney <Qharney@gmail.com>
2015-05-14 13:53:44 -0500

    Initial Commit

---
+++ TicTacToe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTacToe
+//
+//  Created by Quinn Harney on 5/14/15.
+//  Copyright (c) 2015 Quinn Harney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TicTacToe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TicTacToe
+//
+//  Created by Quinn Harney on 5/14/15.
+//  Copyright (c) 2015 Quinn Harney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
